ea2c58b573034b6c8b21068e62f944de38a82968
itsxrp <itsxrproms@gmail.com>
2023-06-27 11:45:33 +0000

    sm8350-common: Bringup Pixys OS
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,7 +77,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/pixys/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest_odm.xml
@@ -182,6 +182,7 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 # SEPolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
+include device/pixys/sepolicy/qcom/sepolicy.mk
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
